927a8d746656fafdea5fac548cbddadcbaa23d91
Sateesh <Sateesh.Yemireddi@ivycomptech.com>
2020-07-03 10:01:22 +0530

    - Create and add Xcode universal project to repo and setup Cocoapods and their dependencies.

---
+++ AboutCanada/AboutCanada/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AboutCanada
+//
+//  Created by Sateesh Yemireddi on 7/3/20.
+//  Copyright © 2020 Company. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
